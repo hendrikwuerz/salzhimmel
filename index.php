2b6adcb213d164ccfb1696226a7d5e088d001cfb
h.m.wuerz@web.de <Hendrik Wuerz>
2015-10-07 00:07:16 +0200

    - added image positioning

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="box layout1">
                 <h2><?php the_title(); ?></h2>
-                <img src="<?php theme(); ?>img/testbild.jpg" alt="testbild">
                 <?php the_content(); ?>
                 <p class="publication-date">Veröffentlicht am <time datetime="<?php echo get_the_date('Y-m-d G:i'); ?>"><?php echo get_the_date('j. F Y'); ?></time></p>
             </div>
--- page.php
+++ page.php
@@ -1,27 +1,17 @@
 <?php get_header(); ?>
 
-    <div id="sidebar">
-        <?php get_sidebar(); ?>
-    </div><!-- sidebar -->
+<?php get_sidebar(); ?>
 
-    <div id="main">
+    <main>
 
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-            <div class="entry">
+            <div class="box layout1">
+                <h2><?php the_title(); ?></h2>
                 <?php the_content(); ?>
+                <p class="publication-date">Veröffentlicht am <time datetime="<?php echo get_the_date('Y-m-d G:i'); ?>"><?php echo get_the_date('j. F Y'); ?></time></p>
             </div>
         <?php endwhile; endif; ?>
 
-        <?php
-        /*
-         * Kommentare sind auf Seiten deaktiviert.
-         * Möchtest du die Kommentarfunktion auf Seiten aktivieren, entferne einfach die beiden "//"-Zeichen vor "comments_template();"
-         */
-
-        //comments_template();
-        ?>
-
-    </div><!-- main -->
+    </main><!-- main -->
 
 <?php get_footer(); ?>
